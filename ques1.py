849a8fb826e1831210d756c345222d3200395373
Stuti Arya <stutiarya@Stutis-MacBook-Air.local>
2024-06-18 00:58:28 +0530

    Added solution for question 1

---
+++ ques1.py
@@ -0,0 +1,9 @@
+# Prompt the user to enter two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Print the sum in a clear message
+print("The sum of", num1, "and", num2, "is", sum)
