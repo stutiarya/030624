62082ad44782a105741fb86d176963a6e00ed4cb
Stuti Arya <stutiarya@Stutis-MacBook-Air.local>
2024-06-18 14:33:33 +0530

    Added ques 17

---
+++ ques17.py
@@ -0,0 +1,8 @@
+# Take input from the user
+string = input("Enter a string: ")
+
+# Convert the string to title case
+title_case_string = string.title()
+
+# Print the title case string
+print("Title case string:", title_case_string)
