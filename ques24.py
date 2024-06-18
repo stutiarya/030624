2832167b57e694bcaa53a41cfeb7b0d7a5496499
Stuti Arya <stutiarya@Stutis-MacBook-Air.local>
2024-06-18 14:49:27 +0530

    Added Ques 24

---
+++ ques24.py
@@ -0,0 +1,20 @@
+# Take input from the user
+num1 = float(input("Enter the first number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+# Perform the calculation based on the operator
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    result = num1 / num2
+else:
+    print("Invalid operator entered.")
+    exit()
+
+# Print the result
+print(f"The result of {num1} {operator} {num2} is: {result}")
