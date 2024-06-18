3cb93fe1e7a691b3e36d3ed884c0f6e6b7f2fe13
Stuti Arya <stutiarya@Stutis-MacBook-Air.local>
2024-06-18 14:02:41 +0530

    Added ques 10

---
+++ ques10.py
@@ -0,0 +1,8 @@
+# Take input from the user
+string = input("Enter a string: ")
+
+# Convert the string to uppercase
+uppercase_string = string.upper()
+
+# Print the uppercase string
+print("Uppercase string:", uppercase_string)
