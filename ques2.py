62742f8e0e405de6519356cbcca672a35f407984
Stuti Arya <stutiarya@Stutis-MacBook-Air.local>
2024-06-18 13:30:25 +0530

    Added solution for ques 2

---
+++ ques2.py
@@ -0,0 +1,8 @@
+# Get the number from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is even using the modulo operator (%)
+if num % 2 == 0:
+  print(num, "is even.")
+else:
+  print(num, "is odd.")
