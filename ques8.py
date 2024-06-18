3e51042d01f620ddb820791e589b1d6cd2cda801
Stuti Arya <stutiarya@Stutis-MacBook-Air.local>
2024-06-18 13:58:46 +0530

    Added Ques 8

---
+++ ques8.py
@@ -0,0 +1,9 @@
+# Take input from the user
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+
+# Concatenate the two strings
+result = string1 + string2
+
+# Print the concatenated string
+print("Concatenated string:", result)
