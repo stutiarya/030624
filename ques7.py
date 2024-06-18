c59e8934640de488ab1c1458ab597bedf439c731
Stuti Arya <stutiarya@Stutis-MacBook-Air.local>
2024-06-18 13:56:19 +0530

    Added ques 7

---
+++ ques7.py
@@ -0,0 +1,8 @@
+# Take input from the user
+string = input("Enter a string: ")
+
+# Calculate the length of the string
+length = len(string)
+
+# Print the length of the string
+print(f"The length of the string is: {length}")
