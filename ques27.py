1ea629571f889b14c4897f4d346468ca1eedb729
Stuti Arya <stutiarya@Stutis-MacBook-Air.local>
2024-06-18 14:54:47 +0530

    Added Ques 27

---
+++ ques27.py
@@ -0,0 +1,8 @@
+# Take input from the user
+string = input("Enter a string: ")
+
+# Convert the string to a list of characters
+characters_list = list(string)
+
+# Print the list of characters
+print("List of characters:", characters_list)
